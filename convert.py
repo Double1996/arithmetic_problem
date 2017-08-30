7d715fa1a7abbb4a8749e60b057c568d10062a49
double <bud2@qq.com>
2017-08-30 10:15:47 +0800

    Array Conver

---
+++ convert.py
@@ -0,0 +1,9 @@
+alist = [[1,1,1],[2,2,2]]
+res = []
+for x in range(len(alist[0])):
+    tem = []
+    for y in range(len(alist)):
+        tem.append(alist[y][x])
+    res.append[tem]
+print(alist)
+print(res)
